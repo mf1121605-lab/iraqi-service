bb3fadcaeb35a4d6b46da9a4e32ddd76cada8c62
mf1121605-lab <mf1121605@gmail.com>
2026-07-29 18:48:59 +0300

    Successfully linked Expo project ID

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 export default ({ config }) => ({
   ...config,
   extra: {
+    ...(config.extra || {}),
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
     supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
   },
